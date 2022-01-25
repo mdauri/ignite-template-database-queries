b9e3e0a878b52b4a3391ad1951817d7b9d5ec651
Márcio Daurí <mdauri@gmail.com>
2022-01-25 00:14:06 -0300

    complete

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,22 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne({where:{id: user_id}, relations:["games"]});
+    if(!user){
+      throw new Error('User does not exists.');
+    }  
+    return user;        
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name ASC`); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    
+    return await this.repository.query(`SELECT * FROM USERS WHERE LOWER(first_name) like LOWER($1) and LOWER(last_name) like LOWER($2)`,[first_name, last_name]); // Complete usando raw query
   }
 }
